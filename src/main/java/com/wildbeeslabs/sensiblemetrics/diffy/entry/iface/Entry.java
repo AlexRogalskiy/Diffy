dc43878a8f20d4461bfd1141abaffc72a66474b6
AlexanderRogalskiy <alexander.rogalskiy@paragon-software.com>
2019-05-14 15:38:02 +0300

    Changeset to project structure

--- src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/entry/iface/Entry.java
+++ src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/entry/iface/Entry.java
@@ -2,6 +2,10 @@ package com.wildbeeslabs.sensiblemetrics.diffy.entry.iface;
 
 import javax.annotation.Nullable;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -40,5 +44,42 @@ public interface Entry<K, V> {
      */
     static <K, V> Collector<Entry<K, V>, ?, Map<K, V>> toMap() {
         return Collectors.toMap(Entry::getKey, Entry::getValue);
+    }
+
+    /**
+     * Invokes the given {@link BiConsumer} if all given {@link Optional} are present.
+     *
+     * @param <K>      type of node key
+     * @param <V>      type of node value
+     * @param key      - initial input key {@link Optional}
+     * @param value    - initial input value {@link Optional}
+     * @param consumer - initial input {@link BiConsumer} operator
+     */
+    static <K, V> void ifAllPresent(final Optional<K> key, final Optional<V> value, final BiConsumer<K, V> consumer) {
+        Objects.requireNonNull(key, "Optional must not be null!");
+        Objects.requireNonNull(value, "Optional must not be null!");
+        Objects.requireNonNull(consumer, "Consumer must not be null!");
+        mapIfAllPresent(key, value, (k, v) -> {
+            consumer.accept(k, v);
+            return null;
+        });
+    }
+
+    /**
+     * Maps the values contained in the given {@link Optional} if both of them are present.
+     *
+     * @param <K>      type of node key
+     * @param <V>      type of node value
+     * @param <R>      type of function result
+     * @param key      - initial input key {@link Optional}
+     * @param value    - initial input value {@link Optional}
+     * @param function - initial input {@link BiFunction} operator
+     * @return {@link Optional} of {@link BiFunction} operator result {@code R}
+     */
+    static <K, V, R> Optional<R> mapIfAllPresent(final Optional<K> key, final Optional<V> value, final BiFunction<K, V, R> function) {
+        Objects.requireNonNull(key, "Optional must not be null!");
+        Objects.requireNonNull(value, "Optional must not be null!");
+        Objects.requireNonNull(function, "BiFunction must not be null!");
+        return key.flatMap(k -> value.map(v -> function.apply(k, v)));
     }
 }
--- src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/entry/impl/DefaultEntry.java
+++ src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/entry/impl/DefaultEntry.java
@@ -3,6 +3,8 @@ package com.wildbeeslabs.sensiblemetrics.diffy.entry.impl;
 import com.wildbeeslabs.sensiblemetrics.diffy.entry.iface.Entry;
 import lombok.*;
 
+import java.util.Optional;
+
 /**
  * Default {@link Entry} implementation
  *
@@ -39,5 +41,19 @@ public class DefaultEntry<K, V> implements Entry<K, V> {
      */
     public static <K, V> Entry<K, V> of(final K first, final V second) {
         return new DefaultEntry<>(first, second);
+    }
+
+    /**
+     * Returns {@link Entry} if both {@link Optional} instances have values or {@link Optional#empty()} if one or both
+     * are missing.
+     *
+     * @param <K>   type of node key
+     * @param <V>   type of node value
+     * @param key   - initial input key {@link Optional}
+     * @param value - initial input value {@link Optional}
+     * @return {@link Optional} of {@link Entry}
+     */
+    public static <K, V> Optional<Entry<K, V>> with(final Optional<K> key, final Optional<V> value) {
+        return key.flatMap(k -> value.map(v -> DefaultEntry.of(k, v)));
     }
 }
