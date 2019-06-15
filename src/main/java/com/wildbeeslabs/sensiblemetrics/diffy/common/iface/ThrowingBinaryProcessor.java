4264688a9529fc9411e0d15d69f9c79093ad1407
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-06-15 06:10:09 +0300

    Changeset to matchers

--- src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/common/iface/ThrowingBinaryProcessor.java
+++ src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/common/iface/ThrowingBinaryProcessor.java
@@ -40,6 +40,17 @@ import static com.wildbeeslabs.sensiblemetrics.diffy.utility.ServiceUtils.doThro
 public interface ThrowingBinaryProcessor<F, T, R, E extends Throwable> {
 
     /**
+     * Default numeric comparators
+     */
+    ThrowingBinaryProcessor<Double, Double, Double, Throwable> DOUBLE_MAX = (n1, n2) -> Double.compare(n1.doubleValue(), n2.doubleValue()) > 0 ? n1 : n2;
+    ThrowingBinaryProcessor<Integer, Integer, Integer, Throwable> INTEGER_MAX = (n1, n2) -> n1.intValue() > n2.intValue() ? n1 : n2;
+    ThrowingBinaryProcessor<Long, Long, Long, Throwable> LONG_MAX = (n1, n2) -> n1.longValue() > n2.longValue() ? n1 : n2;
+    ThrowingBinaryProcessor<Float, Float, Float, Throwable> FLOAT_MAX = (n1, n2) -> Float.compare(n1.floatValue(), n2.floatValue()) > 0 ? n1 : n2;
+    ThrowingBinaryProcessor<Short, Short, Short, Throwable> SHORT_MAX = (n1, n2) -> n1.shortValue() > n2.shortValue() ? n1 : n2;
+    ThrowingBinaryProcessor<Byte, Byte, Byte, Throwable> BYTE_MAX = (n1, n2) -> n1.byteValue() > n2.byteValue() ? n1 : n2;
+    ThrowingBinaryProcessor<Character, Character, Character, Throwable> CHAR_MAX = (n1, n2) -> n1 > n2 ? n1 : n2;
+
+    /**
      * Returns result {@code R} by consuming the supplied arguments {@code F} and {@code T}
      *
      * @param first - initial input first argument {@code F} to be consumed
