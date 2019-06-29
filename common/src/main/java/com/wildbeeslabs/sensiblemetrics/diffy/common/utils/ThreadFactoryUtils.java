9f65daed93630a8f153c5618532fa2eb27208040
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-06-29 18:04:51 +0300

    Changeset to project structure

--- common/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/common/utils/ThreadFactoryUtils.java
+++ common/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/common/utils/ThreadFactoryUtils.java
@@ -23,8 +23,10 @@
  */
 package com.wildbeeslabs.sensiblemetrics.diffy.common.utils;
 
+import com.google.common.base.Supplier;
 import lombok.experimental.UtilityClass;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicLong;
@@ -60,5 +62,70 @@ public class ThreadFactoryUtils {
                 return t;
             }
         };
+    }
+
+    public static <T> Callable<T> threadRenaming(final Callable<T> task, final Supplier<String> nameSupplier) {
+        ValidationUtils.notNull(task, "Callable task should not be null");
+        ValidationUtils.notNull(nameSupplier, "Name supplier should not be null");
+        return () -> {
+            final Thread currentThread = Thread.currentThread();
+            final String oldName = currentThread.getName();
+            final boolean restoreName = trySetName(nameSupplier.get(), currentThread);
+            try {
+                return task.call();
+            } finally {
+                if (restoreName) {
+                    trySetName(oldName, currentThread);
+                }
+            }
+        };
+    }
+
+    /**
+     * Wraps the given runnable such that for the duration of {@link Runnable#run} the thread that is
+     * running with have the given name.
+     *
+     * @param task         The Runnable to wrap
+     * @param nameSupplier The supplier of thread names, {@link Supplier#get get} will be called once
+     *                     for each invocation of the wrapped callable.
+     */
+    public static Runnable threadRenaming(final Runnable task, final Supplier<String> nameSupplier) {
+        ValidationUtils.notNull(task, "Runnable task should not be null");
+        ValidationUtils.notNull(nameSupplier, "Name supplier should not be null");
+        return () -> {
+            final Thread currentThread = Thread.currentThread();
+            final String oldName = currentThread.getName();
+            final boolean restoreName = trySetName(nameSupplier.get(), currentThread);
+            try {
+                task.run();
+            } finally {
+                if (restoreName) {
+                    trySetName(oldName, currentThread);
+                }
+            }
+        };
+    }
+
+    /**
+     * Tries to set name of the given {@link Thread}, returns true if successful.
+     */
+    private static boolean trySetName(final String threadName, final Thread currentThread) {
+        try {
+            currentThread.setName(threadName);
+            return true;
+        } catch (SecurityException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Wraps input {@code T} value in {@link Callable}
+     *
+     * @param <T>   type of value to be wrapped
+     * @param value - initial input {@code T} value
+     * @return {@link Callable}
+     */
+    public static <T> Callable<T> wrap(final T value) {
+        return () -> value;
     }
 }
