a7e1d3f0c6f935dfcfc639000a6dcf48b98d33b6
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-01 04:24:11 +0300

    Changeset to project structure

---
+++ converter/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/converter/service/TimeUnitConverter.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wildbeeslabs.sensiblemetrics.diffy.converter.service;
+
+import com.wildbeeslabs.sensiblemetrics.diffy.common.exception.IllegalFormatConversionException;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Default time unit {@link AbstractConverter} implementation
+ *
+ * @author Alexander Rogalskiy
+ * @version 1.1
+ * @since 1.0
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class TimeUnitConverter extends AbstractConverter<TimeUnit, ChronoUnit> {
+
+    @Override
+    public ChronoUnit valueOf(final TimeUnit timeUnit) {
+        switch (timeUnit) {
+            case NANOSECONDS:
+                return ChronoUnit.NANOS;
+            case MICROSECONDS:
+                return ChronoUnit.MICROS;
+            case MILLISECONDS:
+                return ChronoUnit.MILLIS;
+            case SECONDS:
+                return ChronoUnit.SECONDS;
+            case MINUTES:
+                return ChronoUnit.MINUTES;
+            case HOURS:
+                return ChronoUnit.HOURS;
+            case DAYS:
+                return ChronoUnit.DAYS;
+            default:
+                throw new IllegalFormatConversionException("ERROR: no ChronoUnit equivalent for", TimeUnit.class, timeUnit);
+        }
+    }
+}
