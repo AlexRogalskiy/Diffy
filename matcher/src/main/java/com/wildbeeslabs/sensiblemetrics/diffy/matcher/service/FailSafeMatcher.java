749b7d70d573dd629331140500c364d072d1fbc5
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-01 04:15:11 +0300

    Changeset to project structure

--- matcher/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/matcher/service/FailSafeMatcher.java
+++ matcher/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/matcher/service/FailSafeMatcher.java
@@ -39,6 +39,7 @@ import lombok.ToString;
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@SuppressWarnings("unchecked")
 public class FailSafeMatcher<T> extends AbstractMatcher<T> {
 
     /**
