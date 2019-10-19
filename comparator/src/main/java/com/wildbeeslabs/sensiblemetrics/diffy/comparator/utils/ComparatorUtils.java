f5d660c1b4a499aa6509c18350b363986d3447bd
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-10-19 18:04:55 +0300

    Changeset to project structure

--- comparator/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/comparator/utils/ComparatorUtils.java
+++ comparator/src/main/java/com/wildbeeslabs/sensiblemetrics/diffy/comparator/utils/ComparatorUtils.java
@@ -2722,3 +2722,20 @@ public class ComparatorUtils {
 //            return super.compareParameters(mediaType1, mediaType2);
 //        }
 //    };
+
+//import java.util.Comparator;
+//    import java.util.List;
+//    import java.util.Map.Entry;
+//
+//    import com.tapjoy.reach.params.KeyEnum;
+//
+//public class KeyComparator implements Comparator<Entry<String, List<String>>> {
+//
+//    @Override
+//    public int compare(Entry<String, List<String>> o1,
+//                       Entry<String, List<String>> o2) {
+//        KeyEnum key1 = KeyEnum.getEnum(o1.getKey());
+//        KeyEnum key2 = KeyEnum.getEnum(o2.getKey());
+//        return key1.ordinal() - key2.ordinal();
+//    }
+//}
